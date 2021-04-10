2a70038ad1b4781348cad907ca49fc58a31c782b
Cuddy Steadman <cchsteadman@gmail.com>
2021-04-10 12:28:14 -0400

    fragruns fix + changes to frags command

--- src/main/java/thecudster/sre/commands/FragRun.java
+++ src/main/java/thecudster/sre/commands/FragRun.java
@@ -1,28 +1,31 @@
 package thecudster.sre.commands;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.event.ClickEvent;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatStyle;
+import net.minecraft.util.EnumChatFormatting;
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import thecudster.sre.util.JSONReader;
+import thecudster.sre.util.fragbot.FragBot;
+import thecudster.sre.util.fragbot.FragStatus;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class FragRun implements ICommand {
-    private ArrayList<String> aliases = new ArrayList<String>();
-    public FragRun() {
-        aliases.add("fr");
-        aliases.add("fragruns");
-        aliases.add("frag");
-    }
+public class FragRun extends CommandBase {
+    private String PREFIX = EnumChatFormatting.DARK_RED + "[FragRunner] " + EnumChatFormatting.GRAY;
     @Override
     public String getCommandName() {
         return "fragrun";
@@ -30,40 +33,52 @@ public class FragRun implements ICommand {
 
     @Override
     public String getCommandUsage(ICommandSender sender) {
-        return "Parties an available frag run bot.";
+        return "/frags";
     }
 
     @Override
     public List<String> getCommandAliases() {
-        return aliases;
+        return Arrays.asList("fragbot", "frag", "invitefrag", "fragrun", "fragbots", "fr");
     }
 
     @Override
     public void processCommand(ICommandSender sender, String[] args) throws CommandException {
-        // Minecraft.getMinecraft().thePlayer.sendChatMessage("/p Fragrunners");
-        // Minecraft.getMinecraft().thePlayer.sendChatMessage("/p FraggleRockz");
-        final JSONObject object;
-        try {
-            object = JSONReader.urlToJson(new URL("https://fragrunner.me:3001/fragbots"));
-            JSONArray array = object.getJSONArray("bots");
-            for (int i = 0; i < array.length(); i++) {
-                JSONObject obj = array.getJSONObject(i);
-                if (obj.getString("status").equals("online")) {
-                    Minecraft.getMinecraft().thePlayer.sendChatMessage("/p " + obj.get("name").toString());
+        int amount;
+        if(args.length == 1){
+            amount = Integer.parseInt(args[0]);
+        }else{
+            amount = 1;
+        }
+        new Thread(() -> {
+            try{
+                List<FragBot> fragBots = FragStatus.getBestBot(amount);
+                if(fragBots.size() == 0){
+                    sendMessage("Could not find any Bots. Open a Ticket on our partner's  " +  EnumChatFormatting.UNDERLINE + EnumChatFormatting.AQUA +" Discord");
                     return;
                 }
+                fragBots.forEach((fragBot) -> {
+                    String username = fragBot.getUsername();
+                    Minecraft.getMinecraft().thePlayer.sendChatMessage("/p " + username);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                });
+                sendMessage("These bots are provided by SkyblockReinvented's partner, Fragrunners. You can support them by joining their "
+                        + EnumChatFormatting.UNDERLINE + EnumChatFormatting.AQUA+"Discord!");
+            } catch (MalformedURLException | JSONException e) {
+                sendMessage("There was an Error fetching the Data. Open a Ticket on our Fragrunner's (our partner) "
+                        +EnumChatFormatting.UNDERLINE + EnumChatFormatting.AQUA + "Discord!");
+                e.printStackTrace();
             }
-
-
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+        }).start();
 
     }
 
     @Override
-    public boolean canCommandSenderUseCommand(ICommandSender sender) {
-        return true;
+    public int getRequiredPermissionLevel() {
+        return 0;
     }
 
     @Override
@@ -71,13 +86,9 @@ public class FragRun implements ICommand {
         return null;
     }
 
-    @Override
-    public boolean isUsernameIndex(String[] args, int index) {
-        return false;
-    }
-
-    @Override
-    public int compareTo(@NotNull ICommand o) {
-        return 0;
+    void sendMessage(String text ){
+        Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(PREFIX + text)
+                .setChatStyle(new ChatStyle()
+                        .setChatClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, "https://discord.gg/Tfd6E92MGP"))));
     }
 }
