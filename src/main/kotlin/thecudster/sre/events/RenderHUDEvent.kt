264511303e6e4db884df43e58050eb475da259b5
theCudster <cchsteadman@gmail.com>
2021-07-10 17:16:40 -0400

    forgot some license headers hecc

--- src/main/kotlin/thecudster/sre/events/RenderHUDEvent.kt
+++ src/main/kotlin/thecudster/sre/events/RenderHUDEvent.kt
@@ -3,5 +3,10 @@ package thecudster.sre.events
 import net.minecraftforge.client.event.RenderGameOverlayEvent
 import net.minecraftforge.fml.common.eventhandler.Event
 
-
+/**
+ * Taken from Skytils under GNU Affero General Public license.
+ * https://github.com/Skytils/SkytilsMod/blob/main/LICENSE
+ * @author Sychic
+ * @author My-Name-Is-Jeff
+ */
 class RenderHUDEvent(val event: RenderGameOverlayEvent) : Event()
--- src/main/kotlin/thecudster/sre/util/sbutil/StringUtils.kt
+++ src/main/kotlin/thecudster/sre/util/sbutil/StringUtils.kt
@@ -1,6 +1,11 @@
 package thecudster.sre.util.sbutil
 
-
+/**
+ * Taken from Skytils under GNU Affero General Public license.
+ * https://github.com/Skytils/SkytilsMod/blob/main/LICENSE
+ * @author Sychic
+ * @author My-Name-Is-Jeff
+ */
 import net.minecraft.util.StringUtils as MinecraftStringUtils
 import org.apache.commons.lang3.StringUtils as ApacheStringUtils
 
